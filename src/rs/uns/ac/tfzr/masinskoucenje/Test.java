13d5745cb7a7217489c5300055329d0d70b30372
Alexayy <aleksa.cakic@gmail.com>
2019-03-15 16:01:40 +0100

    Komit 2

--- src/rs/uns/ac/tfzr/masinskoucenje/Test.java
+++ src/rs/uns/ac/tfzr/masinskoucenje/Test.java
@@ -2,17 +2,10 @@ package rs.uns.ac.tfzr.masinskoucenje;
 
 public class Test {
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return super.hashCode();
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
+    int s;
+    int qwe;
 }
