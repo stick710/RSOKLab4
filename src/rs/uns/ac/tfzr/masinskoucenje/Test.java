ec6c04b124b8a8dca8bf425944d28a72fafce182
Alexayy <aleksa.cakic@gmail.com>
2019-03-15 15:45:30 +0100

    Komit 2

---
+++ src/rs/uns/ac/tfzr/masinskoucenje/Test.java
@@ -0,0 +1,18 @@
+package rs.uns.ac.tfzr.masinskoucenje;
+
+public class Test {
+    @Override
+    public int hashCode(){
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
